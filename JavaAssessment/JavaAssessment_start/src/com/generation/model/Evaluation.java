c06b1a09c7fe887895e076f82b00cb67e9f59afb
Marcelchia <62213910+Marcelchia@users.noreply.github.com>
2022-02-26 11:57:21 +0800

    add Map

--- JavaAssessment/JavaAssessment_start/src/com/generation/model/Evaluation.java
+++ JavaAssessment/JavaAssessment_start/src/com/generation/model/Evaluation.java
@@ -1,11 +1,8 @@
 package com.generation.model;
 
-import java.util.List;
+import java.util.Map;
 
 public interface Evaluation
 {
-	double getAverage();
-
-	List<Course> getApprovedCourses();
-
+    Map<String, Course> getEnrolledCourses();
 }
